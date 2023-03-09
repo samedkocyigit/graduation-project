ca7f6d453c9a9443cf7930ccdad24f963a4f8f19
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-03-09 14:45:48 +0300

    created new project

---
+++ Wifi-triangulation/Wifi-triangulation/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+
+
+
+namespace ConsoleApp1
+
+{
+
+    internal class Program
+
+    {
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+
+        public static extern int system(string format);
+
+        static void Main(string[] args)
+
+        {
+
+            string cmd = "netsh wlan show networks mode = bssid";//komut satırı veya powershell için komut
+
+            system(cmd);
+
+
+
+        }
+
+    }
+
+}
